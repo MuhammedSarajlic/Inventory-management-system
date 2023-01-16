3e4627796ade6e4eca80acef6c80f5828913da37
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-16 23:51:11 +0100

    Update employee table

--- client/src/Pages/Employee.jsx
+++ client/src/Pages/Employee.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Employee = () => {
   return (
-    <div className="pt-20 max-w-5xl mx-auto">
+    <div className="pt-20 max-w-7xl mx-auto px-4">
       <p className="">Employee</p>
 
       <div class="relative overflow-x-auto">
@@ -10,16 +10,25 @@ const Employee = () => {
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" class="px-6 py-3">
-                Product name
+                First Name
               </th>
               <th scope="col" class="px-6 py-3">
-                Color
+                Last Name
               </th>
               <th scope="col" class="px-6 py-3">
-                Category
+                Telephone
               </th>
               <th scope="col" class="px-6 py-3">
-                Price
+                Address
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Date of employment
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Date of cancellation
               </th>
             </tr>
           </thead>
@@ -29,33 +38,14 @@ const Employee = () => {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple MacBook Pro 17"
+                Ime
               </th>
-              <td class="px-6 py-4">Sliver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td class="px-6 py-4">White</td>
-              <td class="px-6 py-4">Laptop PC</td>
-              <td class="px-6 py-4">$1999</td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td class="px-6 py-4">Black</td>
-              <td class="px-6 py-4">Accessories</td>
-              <td class="px-6 py-4">$99</td>
+              <td class="px-6 py-4">Prezime</td>
+              <td class="px-6 py-4">Telephone</td>
+              <td class="px-6 py-4">Address</td>
+              <td class="px-6 py-4">Email</td>
+              <td class="px-6 py-4">DoC</td>
+              <td class="px-6 py-4">DoC</td>
             </tr>
           </tbody>
         </table>
